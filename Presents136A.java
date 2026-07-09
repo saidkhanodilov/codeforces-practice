394eec3789fc040ebf6143ab0c47d578661b2587
SaidxonOdilov <saidmukhammadxon@gmail.com>
2026-07-09 22:45:22 +0500

    Presents 136A

--- Presents136A.java
+++ Presents136A.java
@@ -1,3 +1,24 @@
-void main() {
-  $END$
+import java.util.Scanner;
+
+public class Presents136A {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] p = new int[n + 1];
+        int[] answer = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            p[i] = scanner.nextInt();
+            answer[p[i]] = i;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            sb.append(answer[i]);
+            if (i < n) sb.append(' ');
+        }
+        System.out.println(sb.toString());
+
+        scanner.close();
+    }
 }
