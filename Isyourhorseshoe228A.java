a10100f09f722479a15b1ee1d66beef239111a1d
SaidxonOdilov <saidmukhammadxon@gmail.com>
2026-07-09 22:44:53 +0500

    Is Your horses shoe 228A solution

--- Isyourhorseshoe228A.java
+++ Isyourhorseshoe228A.java
@@ -1,3 +1,17 @@
-void main() {
-  $END$
+import java.util.Scanner;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Isyourhorseshoe228A {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Set<Integer> colors = new HashSet<>();
+
+        for (int i = 0; i < 4; i++) {
+            colors.add(scanner.nextInt());
+        }
+
+        System.out.println(4 - colors.size());
+        scanner.close();
+    }
 }
