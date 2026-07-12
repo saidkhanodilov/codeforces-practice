0dd9473f4ea2cd556df05102e766bc010a2ac76c
SaidxonOdilov <saidmukhammadxon@gmail.com>
2026-07-12 20:54:55 +0500

    Add Sum 1742A solution

--- Sum1742A.java
+++ Sum1742A.java
@@ -1,3 +1,24 @@
-void main() {
-  $END$
+import java.util.Scanner;
+
+public class Sum1742A {
+    public static void main(String[] args) {
+        // Take input
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        // Process
+        StringBuilder sb = new StringBuilder();
+        while (t-- > 0) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            int c = scanner.nextInt();
+
+            if (a + b == c || a + c == b || b + c == a) {
+                sb.append("YES\n");
+            } else {
+                sb.append("NO\n");
+            }
+        }
+        // Output
+        System.out.print(sb);
+    }
 }
