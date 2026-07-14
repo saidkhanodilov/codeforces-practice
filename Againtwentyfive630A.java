5d0a4fff1cf718bf473006192b873c3f8b054205
SaidxonOdilov <saidmukhammadxon@gmail.com>
2026-07-14 18:41:51 +0500

    Add Again Twenty five 630A solution

---
+++ Againtwentyfive630A.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Againtwentyfive630A {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // Take input
+        if (scanner.hasNext()) {
+            String n = scanner.next();
+        }
+        // Output
+        System.out.println(25); // It is always 25
+        scanner.close();
+    }
+}
