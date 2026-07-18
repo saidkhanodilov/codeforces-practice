2b984759c974890cf5d7917435c65988e2f623ab
SaidxonOdilov <saidkhan.odilov@gmail.com>
2026-07-18 13:57:20 +0500

    Add Borze32B solution

---
+++ 800/Borze32B.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Borze32B {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) == '.'){
+                result.append('0');
+            }
+            else{
+                if(i+1 < str.length() && str.charAt(i+1) == '.'){
+                    result.append('1');
+                }
+                else{
+                    result.append('2');
+                    i++;
+                }
+            }
+        }
+        System.out.println(result.toString());
+    }
+}
