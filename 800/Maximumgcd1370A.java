ec77744bce6a38da5447ac55797d2836e5bf8796
SaidxonOdilov <saidkhan.odilov@gmail.com>
2026-07-23 20:48:35 +0500

    Add Maximum GCD 1370A solution

---
+++ 800/Maximumgcd1370A.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Maximumgcd1370A {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+
+        while (t-- > 0) {
+            int n = scanner.nextInt();
+            System.out.println(n / 2);
+        }
+
+        scanner.close();
+    }
+}
