7ecce77de16ede602e1cf0f48daa4899bc136208
SaidxonOdilov <saidkhan.odilov@gmail.com>
2026-07-25 19:10:59 +0500

    Add Easy Problem 2044A solution

---
+++ 800/Easyproblem2044A.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Easyproblem2044A {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+
+        while (t-- > 0) {
+            int n = scanner.nextInt();
+            System.out.println(n - 1);
+        }
+        scanner.close();
+    }
+}
