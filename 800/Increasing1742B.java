7320475463fc4f43cffeb79a7e5064082a326d6b
SaidxonOdilov <saidkhan.odilov@gmail.com>
2026-07-26 21:40:27 +0500

    Add Increasing 1742B solution

---
+++ 800/Increasing1742B.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Increasing1742B {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        while (t-- > 0) {
+            int n = scanner.nextInt();
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = scanner.nextInt();
+            Arrays.sort(a);
+            boolean ok = true;
+            for (int i = 1; i < n; i++) {
+                if (a[i] == a[i - 1]) {
+                    ok = false;
+                    break;
+                }
+            }
+            System.out.println(ok ? "YES" : "NO");
+        }
+        scanner.close();
+    }
+}
