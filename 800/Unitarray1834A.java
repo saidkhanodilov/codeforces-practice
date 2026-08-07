7719e1ce19f0241497fea826f43c16f6327eb1cb
SaidxonOdilov <saidkhan.odilov@gmail.com>
2026-08-07 12:30:27 +0500

    Add Unit array 1834A solution

---
+++ 800/Unitarray1834A.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Unitarray1834A {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        if (!scanner.hasNextInt()) {
+            return;
+        }
+
+        int t = scanner.nextInt();
+
+        for (int i = 0; i < t; i++) {
+            int n = scanner.nextInt();
+            int negCount = 0;
+            int posCount = 0;
+
+            for (int j = 0; j < n; j++) {
+                int a = scanner.nextInt();
+                if (a == -1) {
+                    negCount++;
+                } else {
+                    posCount++;
+                }
+            }
+
+            int ops = 0;
+
+            while (negCount > posCount || negCount % 2 != 0) {
+                negCount--;
+                posCount++;
+                ops++;
+            }
+
+            System.out.println(ops);
+        }
+
+        scanner.close();
+    }
+}
