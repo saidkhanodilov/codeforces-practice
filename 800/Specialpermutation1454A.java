6677c6811375ba7d83b93fc45cfe50670cd17eef
SaidxonOdilov <saidkhan.odilov@gmail.com>
2026-08-12 15:21:47 +0500

    Add SpecialPermutation 1454A solution

--- 800/Specialpermutation1454A.java
+++ 800/Specialpermutation1454A.java
@@ -14,20 +14,7 @@ public class Specialpermutation1454A {
             int n = scanner.nextInt();
 
             for (int j = 1; j <= n; j++) {
-                int val;
-                if (n % 2 == 0) {
-                    if (j <= n / 2) {
-                        val = j + n / 2;
-                    } else {
-                        val = j - n / 2;
-                    }
-                } else {
-                    if (j <= n / 2 + 1) {
-                        val = (j == n / 2 + 1) ? 1 : j + n / 2;
-                    } else {
-                        val = (j == n) ? n / 2 + 1 : j - (n / 2 + 1);
-                    }
-                }
+                int val = (j % n) + 1;
                 System.out.print(val + (j == n ? "" : " "));
             }
             System.out.println();
