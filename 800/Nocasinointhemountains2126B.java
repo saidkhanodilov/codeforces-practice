3c79bfab960bd59caf952330b2715d5b6122fdc9
SaidxonOdilov <saidkhan.odilov@gmail.com>
2026-08-26 23:01:58 +0500

    Add No casino in the mountains 2126B solution

--- 800/Nocasinointhemountains2126B.java
+++ 800/Nocasinointhemountains2126B.java
@@ -1,2 +1,48 @@
+
+import java.util.Scanner;
+
 public class Nocasinointhemountains2126B {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        if (!scanner.hasNextInt()) {
+            return;
+        }
+
+        int t = scanner.nextInt();
+
+        for (int tc = 0; tc < t; tc++) {
+            int n = scanner.nextInt();
+            int k = scanner.nextInt();
+
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = scanner.nextInt();
+            }
+
+            int hikes = 0;
+            int i = 0;
+
+            while (i <= n - k) {
+                boolean possible = true;
+                for (int j = 0; j < k; j++) {
+                    if (a[i + j] == 1) {
+                        possible = false;
+                        break;
+                    }
+                }
+
+                if (possible) {
+                    hikes++;
+                    i += k + 1;
+                } else {
+                    i++;
+                }
+            }
+
+            System.out.println(hikes);
+        }
+
+        scanner.close();
+    }
 }
